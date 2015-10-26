38fcb4a026a8fda61b2b74535a129b7aee1726b9
dloknapp <dknapp@chicagobooth.edu>
2015-10-26 17:07:19 -0500

    Photo Edits

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,48 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo_id = params[:id].to_i
+    @photo_source = Photo.find(@photo_id).source
+    @photo_caption = Photo.find(@photo_id).caption
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+    i = Photo.new
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @photo_id = params[:id].to_i
+    i = Photo.find(@photo_id)
+    i.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @photo_id = params[:id].to_i
+    @photo_source = Photo.find(@photo_id).source
+    @photo_caption = Photo.find(@photo_id).caption
+  end
+
+  def update_row
+    @photo_id = params[:id].to_i
+    i = Photo.find(@photo_id)
+    i.source = params[:the_source]
+    i.caption = params[:the_caption]
+    i.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
 
 end
+
--- db/migrate/20141105190754_create_photos.rb
+++ db/migrate/20141105190754_create_photos.rb
@@ -1,4 +1,4 @@
-class CreatePhotos < ActiveRecord::Migration
+       class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :source
